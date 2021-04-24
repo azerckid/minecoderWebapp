71f60c0b25aefb0e3646e7907bd0e1a7034a5159
HENRY <azerckid@gmail.com>
2021-04-24 14:34:42 +0900

    safeArea

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import  * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { WebView } from 'react-native-webview';
+import { StatusBar } from "expo-status-bar";
+import * as React from "react";
+import { ActivityIndicator, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function App() {
+  const [visible, setVisible] = React.useState(false);
+  const ActivityIndicatorElement = () => {
+    return(
+      <View style={styles.ActivityIndicatorStyle}>
+        <ActivityIndicator color="gray" size="large"></ActivityIndicator>
+      </View>
+    )
+  };
   return (
-    <WebView source={{ uri: 'http://mlm.banco.id/' }} style={{ marginTop: 20 }} />
+    <SafeAreaView style={styles.container}>
+      <WebView
+        source={{ uri: "http://mlm.banco.id/" }}
+        style={{ flex: 1 }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        onLoadStart={()=> setVisible(true)}
+        onLoad={()=> setVisible(false)}
+      />
+      {visible? <ActivityIndicatorElement /> : null}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 40,
+    backgroundColor:"#000"
   },
+  ActivityIndicatorStyle:{
+    flex: 1,
+    position: "absolute",
+    marginLeft:"auto",
+    marginTop:"auto",
+    marginRight:"auto",
+    marginBottom:"auto",
+    left:0,
+    top:0,
+    right:0,
+    bottom:0,
+    justifyContent:"center"
+  }
 });
